4ace1ce2898c52c05ad8b86bd07d88b2458a5fa0
Rishabh Ranjan <mr.rishabh.ranjan@gmail.com>
2018-12-14 19:29:43 +0530

    Script 2 initial commit

---
+++ uname.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# @author: Rishabh Ranjan
+# @date: 14-12-2018
+# Dev Club Bash assignment 2
+
+if [[ $# != 2 ]]
+then
+	exit 1
+fi
+
+grep -w "$2" "$1" |
+cut -d : -f 5
+
+if [[ $? ]]
+then
+	exit 1
+fi
